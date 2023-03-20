5d5212e709900f5cc7cfa61bdd6a9c925f207835
Yasir Mohamed <muadnoor.27@gmail.com>
2023-03-20 10:17:14 -0500

    set up get by id for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,23 @@ router.get('/', async(req, res) => {
 }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryDataById = await Category.findByPk(req.params.id, {
+      include: [{ model: Product, as: 'products' }],
+    });
+    
+    // be sure to include its associated Products
+    if (!categoryDataById) {
+        res.status(404).json({ message: 'No category found with that id.' })
+        return;
+    }
+
+    res.status(200).json(categoryDataById);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
