54d9e2b5318df3d933a3a5e7b1d4851047783896
Yasir Mohamed <muadnoor.27@gmail.com>
2023-03-20 10:26:28 -0500

    git by id for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,22 @@ router.get('/', async(req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productDataById = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Category and Tag data
+        include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag, as: 'tagIds' }]
+    });
+
+    if (!productDataById) {
+        res.status(404).json({ message: 'No product found with that id!' });
+        return;
+    }
+    res.status(200).json(productDataById);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 // create new product
