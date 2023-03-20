a2b4b6adb706ec94b35d8f10fe731b94df7c6b67
Yasir Mohamed <muadnoor.27@gmail.com>
2023-03-20 10:19:18 -0500

    setup put route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,24 @@ router.post('/', async(req, res) => {
 }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update({
+        category_name: req.body.category_name,
+    }, {
+        where: {
+            id: req.params.id
+        },
+    });
+    if (!updatedCategory) {
+        res.status(404).json({ message: "No category found with that id!" });
+        return;
+    }
+    res.status(200).json(updatedCategory);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
