3dc5e6c6b9bdf367bcceb1b45ee552af32562259
Yasir Mohamed <muadnoor.27@gmail.com>
2023-03-20 10:34:53 -0500

    delete route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,8 +62,23 @@ router.put('/:id', async(req, res) => {
 }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({
+        where: {
+            id: req.params.id,
+        },
+    });
+    if (!deletedTag) {
+        res.status(404).json({ message: "No tag found with that id!" });
+        return;
+    }
+
+    res.status(200).json(deletedTag);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 module.exports = router;
