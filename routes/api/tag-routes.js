508ab40fffcf9b8b9e55731aed59f4d5e24bfc29
Yasir Mohamed <muadnoor.27@gmail.com>
2023-03-20 10:33:14 -0500

    create route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,15 @@ router.get('/:id', async(req, res) => {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const newTagData = await Tag.create();
+
+    res.status(201).json(newTagData);
+} catch (err) {
+    req.status(500).json(err);
+}
 });
 
 router.put('/:id', (req, res) => {
