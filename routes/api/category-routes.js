b9b3a47d2f6bc3d1e1d9e851ef7fae83b26434d2
Yasir Mohamed <muadnoor.27@gmail.com>
2023-03-20 10:17:58 -0500

    set up post route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,8 +35,15 @@ router.get('/:id', async(req, res) => {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const newCategoryData = await Category.create(req.body);
+
+    res.status(201).json(newCategoryData);
+} catch (err) {
+    res.status(400).json(err);
+}
 });
 
 router.put('/:id', (req, res) => {
